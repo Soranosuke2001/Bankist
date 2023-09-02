5c1909c8b436322dd7e41a53364b942840032ba0
Sora <80365991+Soranosuke2001@users.noreply.github.com>
2023-09-02 15:27:33 -0700

    separates withdrawals and deposits from a single array

--- script.js
+++ script.js
@@ -95,6 +95,21 @@ const createUsernames = accounts => {
 
 createUsernames(accounts);
 
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(movement => {
+  return movement > 0;
+});
+
+const withdrawals = movements.filter(movement => {
+  return movement < 0;
+});
+
+console.log(deposits);
+console.log(withdrawals);
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -104,7 +119,5 @@ const currencies = new Map([
   ["EUR", "Euro"],
   ["GBP", "Pound sterling"],
 ]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
